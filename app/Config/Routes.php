4ded555e750826f8b927959c71a4b835fb9a904c
dimmmslots <vctrline2048@gmail.com>
2023-02-03 15:14:06 +0700

    working on GET endpoints (all, by id/name/price)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/products', 'Product::getAll');
+$routes->get('/products/(:name)', 'Product::getByName/$1');
+$routes->get('/products/(:num)', 'Product::getById/$1');
 
 /*
  * --------------------------------------------------------------------
--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -4,24 +4,73 @@ namespace App\Controllers;
 
 class Product extends BaseController
 {
+    private $productModel;
+    public function __construct()
+    {
+        // init model 
+        $this->productModel = new \App\Models\Product();
+    }
     public function getAll()
     {
-        return 'get all products';
-    }
+        try {
+            $name = $this->request->getGet('name') ? $this->request->getGet('name') : null;
+            $min = $this->request->getGet('min') ? $this->request->getGet('min') : null;
+            $max = $this->request->getGet('max') ? $this->request->getGet('max') : null;
 
-    public function getByName()
-    {
-        return 'get products by search';
-    }
+            // prepared statement
+            $query = $this->productModel->select('*');
+            if ($name) {
+                $query->like('name', $name);
+            }
+            if ($min) {
+                $query->where('price >=', $min);
+            }
+            if ($max) {
+                $query->where('price <=', $max);
+            }
+            $products = $query->get()->getResultArray();
 
-    public function getByPrice()
-    {
-        return 'get products by price';
+            $data = [
+                'message' => 'success',
+                'count' => count($products),
+                'data' => $products
+            ];
+
+            if (count($products) > 0) {
+                return $this->response->setStatusCode(200)->setJSON($products);
+            } else {
+                $empty = [
+                    'message' => 'No products found',
+                    'count' => 0,
+                    'data' => []
+                ];
+                return $this->response->setStatusCode(200)->setJSON($empty);
+            }
+        } catch (\Throwable $th) {
+            return $this->response->setStatusCode(500)->setJSON($th->getMessage());
+        }
     }
 
     public function getById($id)
     {
-        return 'get product by id: ' . $id;
+        try {
+            $product = $this->productModel->find($id);
+            if ($product) {
+                $data = [
+                    'message' => 'found',
+                    'data' => $product
+                ];
+                return $this->response->setStatusCode(200)->setJSON($product);
+            } else {
+                $empty = [
+                    'message' => 'No product found',
+                    'data' => []
+                ];
+                return $this->response->setStatusCode(200)->setJSON($empty);
+            }
+        } catch (\Throwable $th) {
+            return $this->response->setStatusCode(500)->setJSON($th->getMessage());
+        }
     }
 
     public function create()
@@ -38,5 +87,4 @@ class Product extends BaseController
     {
         return 'delete product by id: ' . $id;
     }
-
 }
